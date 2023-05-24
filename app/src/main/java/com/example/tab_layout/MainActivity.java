9a29c047e758171215ba5544f12374c05f481cbe
bhavadip <bhavadipkhunt21@gmail.com>
2023-05-24 15:04:28 +0530

    frist

--- app/src/main/java/com/example/tab_layout/MainActivity.java
+++ app/src/main/java/com/example/tab_layout/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.tab_layout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView=findViewById(R.id.recycle_view);
+        GridLayoutManager layoutManager = new GridLayoutManager(this,2);
+        recyclerView.setLayoutManager(layoutManager);
     }
 }
