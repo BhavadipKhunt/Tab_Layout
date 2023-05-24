fe323e346a7fa320cbae0ce987d760221c3efec2
C22 <bhavadipkhunt21@gmail.com>
2023-05-24 11:57:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tab_layout/Category.java
@@ -0,0 +1,16 @@
+package com.example.tab_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+
+public class Category extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
+}
